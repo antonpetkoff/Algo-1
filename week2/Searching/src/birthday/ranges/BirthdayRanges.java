722fb9637cd950875bb236ac23f43675c5215628
tonynho <tonynho010@gmail.com>
2015-06-18 00:53:10 +0300

    implement BirthdayRanges solution

--- week2/Searching/src/birthday/ranges/BirthdayRanges.java
+++ week2/Searching/src/birthday/ranges/BirthdayRanges.java
@@ -2,20 +2,18 @@ package birthday.ranges;
 
 import java.util.Arrays;
 
-import static utils.Utils.*;
-
 public class BirthdayRanges {
 
     /**
-     * @param   arr
-     * @param   key
-     * @return  the leftmost index of key
+     * @param arr
+     * @param key
+     * @return the leftmost index of key
      */
     public static <T extends Comparable<T>> int searchGreaterOrEquals(T[] arr, T key) {
         int lo = 0, hi = arr.length - 1, mid = 0;
-        
+
         while (hi - lo >= 0) {
-            mid = lo + (hi - lo)/2;
+            mid = lo + (hi - lo) / 2;
             if (arr[mid].compareTo(key) < 0) {
                 lo = mid + 1;
             } else {
@@ -25,63 +23,38 @@ public class BirthdayRanges {
                 hi = mid;
             }
         }
-        
+
         return mid;
     }
-    
-    // return rightmost index of key
-    public static <T extends Comparable<T>> int searchLessOrEquals(T[] arr, T key) {
-        int lo = 0, hi = arr.length - 1, mid = 0;
-        
-        while (hi - lo >= 0) {
-            mid = (hi - lo == 1) ? (1 +lo + (hi - lo)/2) : (lo + (hi - lo)/2); 
-            
-            if (arr[mid].compareTo(key) <= 0) {
-                lo = mid;
-                if (hi - lo == 0) {
-                    return mid;
-                }
-            } else {
-                hi = mid - 1;
-            }
-        }
-        
-        return mid;
-    }
-    
+
     /**
-     * @param   birthdays
-     * @param   ranges
-     * @return  list of integers where for each I in the list I equals the
-     * number of people with birthdays in the range ranges[i].
+     * @param birthdays
+     * @param ranges
+     * @return list of integers where for each I in the list I equals the number
+     *         of people with birthdays in the range ranges[i].
      */
     public static Integer[] birthdaysCount(Integer[] birthdays, Range[] ranges) {
         Integer[] buckets = new Integer[ranges.length];
         int left = 0, right = 0;
         Arrays.sort(birthdays);
-        
+
         for (int i = 0; i < ranges.length; ++i) {
             left = searchGreaterOrEquals(birthdays, ranges[i].left);
-            right = searchLessOrEquals(birthdays, ranges[i].right);
-            
-            buckets[i] = right - left + 1;
-            
+            right = searchGreaterOrEquals(birthdays, ranges[i].right);
+
+            if (birthdays[right].compareTo(ranges[i].right) > 0) {
+                --right;
+            }
+
             if (right == left && !birthdays[left].equals(ranges[i].left)
                     && !birthdays[right].equals(ranges[i].right)) {
                 buckets[i] = 0;
+            } else {
+                buckets[i] = right - left + 1;
             }
         }
-        
+
         return buckets;
     }
-    
-    public static void main(String[] args) {
-        Integer[] birthdays = (Integer[]) Arrays.asList(1, 2, 3, 3, 6, 25, 52).toArray();
-        Range[] ranges = new Range[] {new Range(2, 7), new Range(2, 80), new Range(0, 0), new Range(150, 160)};
-        Integer[] counts = birthdaysCount(birthdays, ranges);
-        
-        printArray(counts);
-        System.out.println();
-    }
-    
+
 }
---
+++ week2/Searching/test/birthday/ranges/TestBirthdayRanges.java
@@ -0,0 +1,38 @@
+package birthday.ranges;
+
+import java.util.Arrays;
+
+import org.junit.Test;
+
+import static birthday.ranges.BirthdayRanges.birthdaysCount;
+import static org.junit.Assert.*;
+
+public class TestBirthdayRanges {
+
+    @Test
+    public void testBirthdayRanges1() {
+        Integer[] birthdays = (Integer[]) Arrays.asList(5, 10, 6, 7, 3, 4, 5, 11, 21, 300, 15).toArray();
+        Range[] ranges = new Range[] { new Range(4, 9), new Range(6, 7), new Range(200, 225), new Range(300, 365) };
+        Integer[] counts = birthdaysCount(birthdays, ranges);
+        
+        assertEquals(4, counts.length);
+        assertEquals(Integer.valueOf(5), counts[0]);
+        assertEquals(Integer.valueOf(2), counts[1]);
+        assertEquals(Integer.valueOf(0), counts[2]);
+        assertEquals(Integer.valueOf(1), counts[3]);
+    }
+    
+    @Test
+    public void testBirthdayRanges2() {
+        Integer[] birthdays = (Integer[]) Arrays.asList(1, 2, 3, 3, 6, 25, 52).toArray();
+        Range[] ranges = new Range[] { new Range(2, 7), new Range(2, 80), new Range(0, 0), new Range(150, 160) };
+        Integer[] counts = birthdaysCount(birthdays, ranges);
+        
+        assertEquals(4, counts.length);
+        assertEquals(Integer.valueOf(4), counts[0]);
+        assertEquals(Integer.valueOf(6), counts[1]);
+        assertEquals(Integer.valueOf(0), counts[2]);
+        assertEquals(Integer.valueOf(0), counts[3]);
+    }
+
+}
