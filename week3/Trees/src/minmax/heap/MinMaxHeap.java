464c34d713724414969e4354209b1ea5b3dc4740
tonynho <tonynho010@gmail.com>
2015-06-22 20:42:33 +0300

    isMinMaxHeap

--- week3/Trees/src/minmax/heap/MinMaxHeap.java
+++ week3/Trees/src/minmax/heap/MinMaxHeap.java
@@ -15,8 +15,27 @@ public class MinMaxHeap {
     }
 
     // Checks if a binary tree is a min/max heap.
-    public boolean isMinMax(Node root) {
-        //TODO
-        return false;
+    public static boolean isMinMax(Node root) {
+        return isMinMaxLevel(root, 1);
+    }
+    
+    public static boolean isMinMaxLevel(Node root, int level) {
+        if (root == null) {
+            return true;
+        }
+        
+        if (level % 2 != 0) {
+            if (root.left != null && root.value > root.left.value
+                    || root.right != null && root.value > root.right.value) {
+                return false;
+            }
+        } else {
+            if (root.left != null && root.value < root.left.value
+                    || root.right != null && root.value < root.right.value) {
+                return false;
+            }
+        }
+        
+        return isMinMaxLevel(root.left, level + 1) && isMinMaxLevel(root.right, level + 1);
     }
 }
