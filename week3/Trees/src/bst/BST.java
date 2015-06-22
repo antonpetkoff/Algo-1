f72e6f7814e435eeb574bf11faff51456ffc311b
tonynho <tonynho010@gmail.com>
2015-06-22 20:18:18 +0300

    fix isBST

--- week3/Trees/src/bst/BST.java
+++ week3/Trees/src/bst/BST.java
@@ -14,60 +14,22 @@ public class BST {
             this.right = right;
         }
     }
-
-    public static boolean isLeftChildGreater(Node root) {
-        return root.left != null && root.left.value > root.value;
-    }
-    
-    public static boolean isRightChildSmaller(Node root) {
-        return root.right != null && root.right.value < root.value;
-    }
-    
-    public static boolean isRightChildOverRightBound(Node root, int rightBound) {
-        return root.right != null && root.right.value > rightBound;
-    }
-
-    public static boolean isLeftChildOverLeftBound(Node root, int leftBound) {
-        return root.left != null && root.left.value < leftBound;
-    }
-    
-    public static boolean isBSTleft(Node root, int rightBound) {
-        if (root == null) {
-            return true;
-        }
-        
-        if (isLeftChildGreater(root) || isRightChildSmaller(root)
-                || isRightChildOverRightBound(root, rightBound)) {
-            return false;
-        }
-        
-        return isBSTleft(root.left, root.value) && isBSTright(root.right, root.value);
-    }
-    
-    public static boolean isBSTright(Node root, int leftBound) {
-        if (root == null) {
-            return true;
-        }
-        
-        if (isLeftChildGreater(root) || isRightChildSmaller(root)
-                || isLeftChildOverLeftBound(root, leftBound)) {
-            return false;
-        }
-        
-        return isBSTleft(root.left, root.value) && isBSTright(root.right, root.value);
-    }
     
     // Checks if a binary tree is a binary search tree.    
     public static boolean isBST(Node root) {
+        return isBSTRange(root, Integer.MIN_VALUE, Integer.MAX_VALUE);
+    }
+
+    public static boolean isBSTRange(Node root, int left, int right) {
         if (root == null) {
             return true;
         }
-
-        if (isLeftChildGreater(root) || isRightChildSmaller(root)) {
-            return false;
+        
+        if (root.value > left && root.value < right) {
+            return isBSTRange(root.left, left, root.value) && isBSTRange(root.right, root.value, right);
         }
-
-        return isBSTleft(root.left, root.value) && isBSTright(root.right, root.value);
+        
+        return false;
     }
-
+    
 }
---
+++ week3/Trees/src/minmax/heap/MinMaxHeap.java
@@ -0,0 +1,22 @@
+package minmax.heap;
+
+public class MinMaxHeap {
+
+    public static class Node {
+        public int value;
+        public Node left;
+        public Node right;
+
+        public Node(int value, Node left, Node right) {
+            this.value = value;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    // Checks if a binary tree is a min/max heap.
+    public boolean isMinMax(Node root) {
+        //TODO
+        return false;
+    }
+}
