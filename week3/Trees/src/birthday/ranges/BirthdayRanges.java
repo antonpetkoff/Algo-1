9b480f0585d73614c8489cd51eded9ca9d064b2c
tonynho <tonynho010@gmail.com>
2015-06-25 20:40:27 +0300

    birthday ranges

--- week3/Trees/src/birthday/ranges/BirthdayRanges.java
+++ week3/Trees/src/birthday/ranges/BirthdayRanges.java
@@ -2,6 +2,32 @@ package birthday.ranges;
 
 public class BirthdayRanges {
 
+    private BinaryIndexedTree bit;
     
+    public BirthdayRanges(int[] values) {
+        bit = new BinaryIndexedTree(365, values);
+    }
     
+    // adds people who are born on a specific day
+    public void add(int day, int numberOfPeople) {
+        bit.update(day, numberOfPeople);
+    }
+
+    // removes people who are born on a specific day
+    public void remove(int day, int numberOfPeople) {
+        bit.update(day, -numberOfPeople);
+    }
+
+    // returns the number of people born in a range
+    public int count(int startDay, int endDay) {
+        int a = bit.query(startDay), b = bit.query(endDay);
+        return a != 0 ? b - a + 1 : b - a;
+    }
+    
+    public static void main(String[] args) {
+        int values[] = {5, 10, 6, 7, 3, 4, 5, 11, 21, 300, 15};
+        BirthdayRanges br = new BirthdayRanges(values);
+        System.out.println(br.count(2, 10));
+    }
+
 }
---
+++ week3/Trees/test/birthday/ranges/TestBirthdayRanges.java
@@ -0,0 +1,29 @@
+package birthday.ranges;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestBirthdayRanges {
+
+    @Test
+    public void testBirthdayRanges() {
+        int values[] = {5, 10, 6, 7, 3, 4, 5, 11, 21, 300, 15};
+        BirthdayRanges br = new BirthdayRanges(values);
+        assertEquals(7, br.count(2, 10));
+        assertEquals(5, br.count(10, 365));
+        
+        br.add(8, 3);
+        assertEquals(6, br.count(7, 11));
+        
+        br.remove(8, 2);
+        assertEquals(4, br.count(7, 11));
+
+        br.add(18, 5);
+        assertEquals(8, br.count(10, 20));
+        
+        br.add(5, 1);
+        assertEquals(13, br.count(5, 25));      // 15
+    }
+    
+}
