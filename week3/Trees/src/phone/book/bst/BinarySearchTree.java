d5af45ec0009a1d5a2fb828bf542f5093bd9f463
tonynho <tonynho010@gmail.com>
2015-06-25 15:17:23 +0300

    implement binary search tree removal

--- week3/Trees/src/phone/book/bst/BinarySearchTree.java
+++ week3/Trees/src/phone/book/bst/BinarySearchTree.java
@@ -72,12 +72,119 @@ public class BinarySearchTree<T extends Comparable<T>> {
         traverseNodes(root);
     }
     
+    private Node<T> findNode(T elem) {
+        Node<T> current = root;
+        
+        while (current != null) {
+            if (elem.compareTo(current.item) < 0) {
+                current = current.left;
+            } else if (elem.compareTo(current.item) > 0) {
+                current = current.right;
+            } else {
+                return current;
+            }
+        }
+        
+        return null;
+    }    
+    
+    public T search(T elem) {
+        Node<T> temp = findNode(elem);
+        return temp == null ? null : temp.item;
+    }
+    
+    private Node<T> leftMostChild(Node<T> root) {
+        Node<T> current = root;
+        
+        while (current != null) {
+            current = current.left;
+        }
+        
+        return current;
+    }
+    
+    private boolean hasNoChildren(Node<T> node) {
+        return node.left == null && node.right == null;
+    }
+    
+    private boolean hasOneChild(Node<T> node) {
+        return (node.left != null && node.right == null) || (node.left == null && node.right != null);
+    }
+    
+    private Node<T> onlyChild(Node<T> node) {
+        return node.left != null ? node.left : node.right;
+    }
+    
+    private Node<T> getParent(Node<T> node) {
+        Node<T> current = root, parent = null;
+        
+        while (current != null) {
+            if (node.item.compareTo(current.item) < 0) {
+                parent = current;
+                current = current.left;
+            } else if (node.item.compareTo(current.item) > 0) {
+                parent = current;
+                current = current.right;
+            } else {
+                break;
+            }
+        }
+        
+        return parent;
+    }
+    
+    private void removeFromParent(Node<T> node) {
+        Node<T> parent = getParent(node);
+        
+        if (parent.left == node) {
+            parent.left = null;
+        } else if (parent.right == node) {
+            parent.right = null;
+        }
+    }
+    
+    private void replaceInParent(Node<T> unneeded, Node<T> replacement) {
+        Node<T> parent = getParent(unneeded);
+        
+        if (parent.left == unneeded) {
+            parent.left = replacement;
+        } else if (parent.right == unneeded) {
+            parent.right = replacement;
+        }
+    }
+    
+    private void removeNode(Node<T> node) {
+        if (node == null) {
+            return;
+        }
+        
+        if (hasNoChildren(node)) {
+            removeFromParent(node);
+        } else if (hasOneChild(node)) {
+            replaceInParent(node, onlyChild(node));
+        } else {
+            Node<T> replacement = leftMostChild(node.right);
+            node.item = replacement.item;
+            removeNode(replacement);
+        }
+    }
+    
+    public void remove(T elem) {
+        Node<T> current = findNode(elem);
+        removeNode(current);
+    }
+    
     public static void main(String[] args) {
         BinarySearchTree<Integer> bst = new BinarySearchTree<Integer>();
         bst.insert(5);
         bst.insert(2);
         bst.insert(7);
+        bst.insert(3);
         bst.traverse();
+        System.out.println();
+        bst.remove(2);
+        bst.traverse();
+        System.out.println(bst.search(4));
     }
     
 }
