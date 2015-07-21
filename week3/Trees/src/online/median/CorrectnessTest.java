2cb157f00b0afafd5452b9ab941a4f914a869b4c
tonynho <tonynho010@gmail.com>
2015-07-21 22:02:43 +0300

    test correctness of Online Median

---
+++ week3/Trees/src/online/median/CorrectnessTest.java
@@ -0,0 +1,56 @@
+package online.median;
+
+import java.util.Random;
+
+public class CorrectnessTest {
+
+    private static Median incorrect;
+    private static SortedSequence correct;
+    
+    private static final int TESTS = 1000;
+    private static final int ITERATIONS = 15;
+    private static final int BOUND = 30;
+    
+    private static boolean foundFlag = false;
+    
+    public static String testCase() {
+        incorrect = new Median();
+        correct = new SortedSequence();
+        Random rand = new Random();
+        StringBuilder msg = new StringBuilder();
+        StringBuilder correctMedians = new StringBuilder();
+        
+        int[] numbers = new int[ITERATIONS];
+        int incorrectMedian = 0;
+        for (int i = 0; i < ITERATIONS; ++i) {
+            numbers[i] = rand.nextInt(BOUND);
+            incorrectMedian = incorrect.insert(numbers[i]);
+            correct.add(numbers[i]);
+            correctMedians.append(Integer.toString((int) correct.getMedian()) + " ");
+            if (incorrectMedian != correct.getMedian()) {
+                foundFlag = true;
+                msg.append(i + " iteration missmatch, number " + numbers[i]);
+                msg.append(" the corect is " + correct.getMedian() + " rather than " + incorrectMedian + '\n');
+            }
+        }
+        
+        msg.append("Numbers used: ");
+        for (int i = 0; i < ITERATIONS; ++i) {
+            msg.append(numbers[i] + " ");
+        }
+        
+        return msg.toString() + "\n" + correctMedians.toString();
+    }
+    
+    public static void main(String[] args) {
+        String msg = null;
+        for (int i = 0; i < TESTS; ++i) {
+            msg = testCase();
+            if (foundFlag) {
+                System.out.println(msg);
+                foundFlag = false;
+            }
+        }
+    }
+    
+}
---
+++ week3/Trees/src/online/median/SortedSequence.java
@@ -0,0 +1,150 @@
+package online.median;
+
+import java.util.Iterator;
+
+public class SortedSequence implements Iterable<Integer> {
+    
+    protected class ListNode {
+        int value;
+        ListNode prev;
+        ListNode next;
+        
+        ListNode(int value) {
+            this.value = value;
+        }
+        
+        ListNode(int value, ListNode prev, ListNode next) {
+            this.value = value;
+            this.prev = prev;
+            this.next = next;
+        }
+    }
+    
+    private ListNode first;
+    private ListNode last;
+    private int size;
+    
+    private ListNode middle;
+    private int leftSize;
+    private int rightSize;
+    
+    SortedSequence() {
+        first = last = middle = null;
+        size = leftSize = rightSize = 0;
+    }
+    
+    public boolean isEmpty() {
+        return first == null;
+    }
+    
+    public int size() {
+        return size;
+    }
+    
+    public Integer getMinimum() {
+        return first != null ? first.value : null;
+    }
+    
+    public Integer getMaximum() {
+        return last != null ? last.value : null;
+    }
+    
+    public int getMedian() {
+        if (size == 0) {
+            return 0;
+        }
+        return middle.value;
+    }
+    
+    public void add(Integer elem) {
+        if (first == null) {
+            first = last = middle = new ListNode(elem, null, null);
+            ++size;
+            leftSize = rightSize = 0;
+            return;
+        }
+        
+        // maintain sorted invariant
+        ListNode temp = last;
+        while ((temp != null) && (temp.value > elem)) {
+            temp = temp.prev;
+        }
+        
+        if (temp == null) {             // add before first node
+            linkNodeAsFirst(elem);
+        } else {                        // add after temp node or last node
+            linkNodeAfter(temp, elem);
+        }
+        
+        ++size;
+        
+        if (elem < middle.value) {
+            ++leftSize;
+        } else {
+            ++rightSize;
+        }
+        
+        // maintain middle node invariant
+        if (leftSize < rightSize) {
+            ++leftSize;
+            --rightSize;
+            middle = middle.next;
+        } else if (leftSize - 1 > rightSize) {
+            --leftSize;
+            ++rightSize;
+            middle = middle.prev;
+        }
+        // else invariant is maintained
+        
+    }
+    
+    private void linkNodeAsFirst(Integer elem) {
+        ListNode newNode = new ListNode(elem, null, first);
+        first.prev = newNode;
+        first = newNode;
+    }
+    
+    private void linkNodeAfter(ListNode pivot, Integer elem) {
+        ListNode newNode = new ListNode(elem);
+        newNode.prev = pivot;
+        
+        if (pivot.next != null) {
+            newNode.next = pivot.next;
+            pivot.next.prev = newNode;
+        } else {
+            newNode.next = null;
+            last = newNode;
+        }
+        
+        pivot.next = newNode;
+    }
+    
+    @Override
+    public Iterator<Integer> iterator() {
+        return new SortedListIterator();
+    }
+    
+    private class SortedListIterator implements Iterator<Integer> {
+
+        private ListNode head = first;
+        
+        @Override
+        public boolean hasNext() {
+            return head != null;
+        }
+
+        @Override
+        public Integer next() {
+            Integer value = head.value;
+            head = head.next;
+            return value;
+        }
+
+        @Override
+        public void remove() {
+            // no need for remove
+        }
+        
+    }
+    
+}
