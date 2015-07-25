7810a7af0d38f1547e3a42ecaac8d38febedc530
tonynho <tonynho010@gmail.com>
2015-07-25 04:00:49 +0300

    reimplement isMinMaxHeap

--- week3/Trees/src/minmax/heap/MinMaxHeap.java
+++ week3/Trees/src/minmax/heap/MinMaxHeap.java
@@ -1,41 +1,57 @@
 package minmax.heap;
 
+import java.util.Scanner;
+
 public class MinMaxHeap {
+    
+    public static int[] heap;
+    public static int size;
 
-    public static class Node {
-        public int value;
-        public Node left;
-        public Node right;
-
-        public Node(int value, Node left, Node right) {
-            this.value = value;
-            this.left = left;
-            this.right = right;
-        }
-    }
-
-    // Checks if a binary tree is a min/max heap.
-    public static boolean isMinMax(Node root) {
-        return isMinMaxLevel(root, 1);
+    public static int left(int i) {
+        return 2 * i + 1;
     }
     
-    public static boolean isMinMaxLevel(Node root, int level) {
-        if (root == null) {
+    public static int right(int i) {
+        return 2 * i + 2;
+    }
+    
+    public static String isMinMax() {
+        return isMinMaxLevel(0, 1) ? "YES" : "NO";
+    }
+    
+    public static boolean isMinMaxLevel(int root, int level) {
+        if (root >= heap.length) {
             return true;
         }
         
         if (level % 2 != 0) {
-            if (root.left != null && root.value > root.left.value
-                    || root.right != null && root.value > root.right.value) {
+            if (left(root) < size && heap[root] > heap[left(root)]
+                    || right(root) < size && heap[root] > heap[right(root)]) {
                 return false;
             }
         } else {
-            if (root.left != null && root.value < root.left.value
-                    || root.right != null && root.value < root.right.value) {
+            if (left(root) < size && heap[root] < heap[left(root)]
+                    || right(root) < size && heap[root] < heap[right(root)]) {
                 return false;
             }
         }
         
-        return isMinMaxLevel(root.left, level + 1) && isMinMaxLevel(root.right, level + 1);
+        return isMinMaxLevel(left(root), level + 1) && isMinMaxLevel(right(root), level + 1);
     }
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        size = scanner.nextInt();
+        heap = new int[size];
+        
+        for (int i = 0; i < size; i++) {
+            heap[i] = scanner.nextInt();
+        }
+        
+        System.out.println(isMinMax());
+        
+        scanner.close();
+    }
+    
 }
