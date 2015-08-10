2c968318f34f65174d836e93f6794d8477fd03da
tonynho <tonynho010@gmail.com>
2015-08-10 18:37:39 +0300

    kpaths

--- week8/Dynamic/src/kpaths/KPaths.java
+++ week8/Dynamic/src/kpaths/KPaths.java
@@ -1,7 +1,31 @@
 package kpaths;
 
+import java.util.List;
+import java.util.Scanner;
+import java.util.Stack;
+
 public class KPaths {
 
+    public static final int SIZE = 1000;
+    public static boolean[][] matrix = new boolean[SIZE][SIZE];
     
+//    public static List<Integer> topologicalSort() {
+//        boolean[] visited = new boolean[SIZE];
+//        Stack<Integer> stack = new Stack<Integer>();
+//        
+//    }
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int edgesCount = scanner.nextInt();
+        
+        for (int i = 0; i < edgesCount; ++i) {
+            matrix[scanner.nextInt()][scanner.nextInt()] = true;
+        }
+        
+        
+        scanner.close();
+    }
     
 }
