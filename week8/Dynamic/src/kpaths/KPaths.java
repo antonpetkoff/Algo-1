ba3eee0a132ad6b8f77ccc3e20de566ef8d92b03
tonynho <tonynho010@gmail.com>
2015-08-13 18:39:24 +0300

    K-paths topo sort

--- week8/Dynamic/src/kpaths/KPaths.java
+++ week8/Dynamic/src/kpaths/KPaths.java
@@ -1,29 +1,78 @@
 package kpaths;
 
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
-import java.util.Stack;
 
 public class KPaths {
 
+    public static final int WHITE = 0;
+    public static final int GREY = 1;
+    public static final int BLACK = 2;
     public static final int SIZE = 1000;
-    public static boolean[][] matrix = new boolean[SIZE][SIZE];
     
-//    public static List<Integer> topologicalSort() {
-//        boolean[] visited = new boolean[SIZE];
-//        Stack<Integer> stack = new Stack<Integer>();
-//        
-//    }
+    public static int[] visited;
+    public static LinkedList<Integer> sorted;
+    
+    public static LinkedList<Integer> topologicalSort(Map<Integer, List<Integer>> graph) {
+        visited = new int[graph.size()];
+        sorted = new LinkedList<>();
+        
+        for (Integer node : graph.keySet()) {
+            if (visited[node] == WHITE) {
+                visit(graph, node);
+            }
+        }
+        
+        return sorted;
+    }
+    
+    public static void visit(Map<Integer, List<Integer>> graph, int node) {
+        if (visited[node] == GREY) {
+            return;
+        }
+        
+        if (visited[node] == WHITE) {
+            visited[node] = GREY;
+            
+            for (Integer neighbour : graph.keySet()) {
+                if (visited[neighbour] == WHITE) {
+                    visit(graph, neighbour);
+                }
+            }
+            
+            visited[node] = BLACK;
+            sorted.addFirst(node);
+        }
+    }
+    
+    public static void putIfAbsent(Map<Integer, List<Integer>> map, int key, int value) {
+        if (!map.containsKey(key)) {
+            map.put(key, new LinkedList<Integer>());
+        }
+        map.get(key).add(value);
+    }
     
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         
         int edgesCount = scanner.nextInt();
+        Map<Integer, List<Integer>> children = new HashMap<>(),
+                parents = new HashMap<>();
         
+        int p, q = 0;
         for (int i = 0; i < edgesCount; ++i) {
-            matrix[scanner.nextInt()][scanner.nextInt()] = true;
+            p = scanner.nextInt();
+            q = scanner.nextInt();
+            putIfAbsent(children, p, q);
+            putIfAbsent(parents, q, p);
         }
         
+        int start = scanner.nextInt(), end = scanner.nextInt(), length = scanner.nextInt();
+        LinkedList<Integer> result = topologicalSort(children);
+        System.out.println(result);
         
         scanner.close();
     }
