f1155fdafa23b114e4944f3beea7201dce198c6a
tonynho <tonynho010@gmail.com>
2015-08-14 16:36:05 +0300

    hash table

--- week6/Hashing/src/hash/table/HashTable.java
+++ week6/Hashing/src/hash/table/HashTable.java
@@ -1,87 +1,152 @@
 package hash.table;
 
-import java.util.Arrays;
-import java.util.List;
+public class HashTable<K, V> {
 
-public class HashTable {
+    public static class Entry<K, V> {
+        public K key;
+        public V value;
+        public Entry<K, V> next;
 
-    private static class Entry {
-        public Integer key;
-        public Integer value;
-        public Entry next;
-
-        public Entry(Integer key, Integer value, Entry next) {
+        public Entry(K key, V value, Entry<K, V> next) {
             this.key = key;
             this.value = value;
             this.next = next;
         }
+        
+        @Override
+        public int hashCode() {
+        	return key.hashCode() ^ value.hashCode();
+        }
+        
+        @Override
+        public boolean equals(Object obj) {
+        	if (this == obj) {
+        		return true;
+        	}
+        	if (obj instanceof Entry) {
+				Entry<?, ?> entry = (Entry<?, ?>) obj;
+				return key.equals(entry.key) && value.equals(entry.value);
+			}
+        	return false;
+        }
+        
+        @Override
+        public String toString() {
+        	return key + "=" + value;
+        }
     }
 
-    private static final int INIT_CAPACITY = 16;
-    private static final float LOAD_FACTOR = 0.75f;
-    private static final float GROWTH_FACTOR = 2.0f;
+    public static final int INIT_CAPACITY = 16;
+    public static final float LOAD_FACTOR = 0.75f;
+    public static final float GROWTH_FACTOR = 2.0f;
 
-    private List<Entry> buckets;
+    private Entry<K, V>[] table;
     private int capacity;
     private int size;
 
-    public HashTable() {
-        buckets = Arrays.asList(new Entry[INIT_CAPACITY]);
-        capacity = INIT_CAPACITY;
-        size = 0;
+
+    @SuppressWarnings("unchecked")
+	public HashTable() {
+    	capacity = INIT_CAPACITY;
+    	size = 0;
+    	table = (Entry<K, V>[]) new Entry[capacity];
     }
 
     public int size() {
         return size;
     }
 
-    public void put(Integer key, Integer value) {
-        if (size / capacity >= LOAD_FACTOR) {
+    public void put(K key, V value) {
+        if (size / (float) capacity >= LOAD_FACTOR) {
             resize(Math.round(capacity * GROWTH_FACTOR));
         }
 
-        Entry newEntry = new Entry(key, value, null);
-        putAtIndex(buckets, indexFor(key), newEntry);
+        Entry<K, V> newEntry = new Entry<K, V>(key, value, null);
+        putAtIndex(table, indexFor(key), newEntry);
         ++size;
     }
 
     public void resize(int newCapacity) {
-        List<Entry> newBuckets = Arrays.asList(new Entry[newCapacity]);
+    	@SuppressWarnings("unchecked")
+		Entry<K, V>[] newTable = (Entry<K, V>[]) new Entry[newCapacity];
         capacity = newCapacity;
 
-        for (int i = 0; i < buckets.size(); ++i) {
-            Entry entry = buckets.get(i), newEntry = null;
+        for (int i = 0; i < table.length; ++i) {
+            Entry<K, V> entry = table[i], newEntry = null;
             while (entry != null) {
-                newEntry = new Entry(entry.key, entry.value, null);
-                putAtIndex(newBuckets, indexFor(entry.key), newEntry);
+                newEntry = new Entry<K, V>(entry.key, entry.value, null);
+                putAtIndex(newTable, indexFor(entry.key), newEntry);
                 entry = entry.next;
             }
         }
 
-        buckets = newBuckets;
+        table = newTable;
     }
 
-    public int indexFor(Integer elem) {
-        return Math.abs(elem.hashCode()) % capacity;
+    public int indexFor(K key) {
+        return Math.abs(key.hashCode()) % capacity;
     }
 
-    public void putAtIndex(List<Entry> buckets, int index, Entry newEntry) {
-        Entry entry = buckets.get(index), prev = entry;
+    public void putAtIndex(Entry<K, V>[] table, int index, Entry<K, V> newEntry) {
+        Entry<K, V> entry = table[index], prev = entry;
 
         if (prev != null) {
-            Entry temp = prev.next;
+            Entry<K, V> temp = prev.next;
             while (temp != null) {
                 temp = temp.next;
             }
             temp = newEntry;
             prev.next = temp;
         } else {
-            buckets.set(index, newEntry);
+            table[index] = newEntry;
         }
     }
 
-    public boolean contains(Integer elem) {
-        return buckets.get(indexFor(elem)) != null;
+    public V get(K key) {
+    	Entry<K, V> result = table[indexFor(key)];
+    	
+    	if (result != null) {
+    		Entry<K, V> temp = result;
+    		while (temp != null && !temp.key.equals(key)) {
+    			temp = temp.next;
+    		}
+    		result = temp;
+    	} else {
+    		result = null;
+    	}
+    	
+    	return result.value;
+    }
+    
+    public boolean contains(K key) {
+    	return get(key) != null;
+    }
+    
+    // XXX: TODO
+    public void remove(K key) {
+    	int index = indexFor(key);
+    	Entry<K, V> entry = table[index];
+    	
+    	if (entry != null) {
+    		if (entry.next == null) {
+    			table[index] = null;
+    			--size;
+    		} else {
+    			Entry<K, V> previous = entry, temp = entry.next;
+    			while (temp != null && !temp.key.equals(key)) {
+    				previous = temp;
+    				temp = temp.next;
+    			}
+    			if (temp != null && key.equals(temp.key)) {
+    				if (temp.next != null) {	// previous -> temp -> another entry
+    					previous.next = temp.next;
+    				} else {					// previous -> temp -> null (end of list)
+    					previous.next = null;
+    				}
+    				--size;
+    			}
+    		}
+    	}
     }
 
 }
--- week6/Hashing/test/hash/table/TestHashTable.java
+++ week6/Hashing/test/hash/table/TestHashTable.java
@@ -2,6 +2,8 @@ package hash.table;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 import org.junit.Before;
@@ -9,25 +11,39 @@ import org.junit.Test;
 
 public class TestHashTable {
 
-    private HashTable ht;
+    private HashTable<Integer, Integer> ht;
     
     @Before
     public void setUp() {
-        ht = new HashTable();
+        ht = new HashTable<>();
     }
     
     @Test
-    public void testSize() {
+    public void testPutFindContainsSize() {
+    	final int COUNT = 1000000;
         assertEquals(Integer.valueOf(0), Integer.valueOf(ht.size()));
         
+        List<Integer> keys = new ArrayList<>();
         Random rand = new Random();
         int temp = 0;
-        for (int i = 0; i < 10000; ++i) {
+        for (int i = 0; i < COUNT; ++i) {
             temp = rand.nextInt();
+            keys.add(temp);
             ht.put(temp, 1);
             assertEquals(Integer.valueOf(i+1), Integer.valueOf(ht.size()));
+            assertEquals(Integer.valueOf(1), ht.get(temp));
             assertTrue(ht.contains(temp));
         }
+        
+//        for (int i = COUNT; i > 0; --i) {
+//        	temp = rand.nextInt(keys.size());
+//        	ht.remove(keys.get(temp));
+//        	assertEquals(Integer.valueOf(i - 1), Integer.valueOf(ht.size()));
+//        	assertFalse(ht.contains(keys.get(temp)));
+//        	keys.remove(temp);
+//        }
+//
+//        assertEquals(Integer.valueOf(0), Integer.valueOf(ht.size()));
     }
 
 }
